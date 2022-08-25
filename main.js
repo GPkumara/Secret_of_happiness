4219a4c1924070610ecb9a819a17163b616861ce
GPkumara <102418094+GPkumara@users.noreply.github.com>
2022-08-25 19:36:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,67 @@
 import http from 'http'
-import url from 'url'
-// post 
-let database=[]
+import pkg from 'pg'
+const Client = pkg.Client
+const client = new Client({
+    user:'secret',
+    host:'localhost',
+    database:'kumaran',
+    password:'root',
+    port:5432
+})
+client.connect((err)=>{
+    if(err) throw err;
+    console.log("connected");
+})
+// create-record
 let user;
 let user1;
 http.createServer(function(req,res){
-    if(req.method == "POST" && req.url == "/create-user"){
+    if(req.method == "POST" && req.url == "/create-record")
+    {
         req.setEncoding("utf-8")
         req.on("data",(data)=>{
             user=data;
         });
         req.on("end",()=>{
             user=JSON.parse(user)
-            database.push(user);
-            console.log(database); 
+            let today = new Date();
+            let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            let time = today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+            let dt=date+' '+time;
+            let values=[user.family,user.field,user.fun,user.finance,user.fitness,user.faith,user.friends,dt];
+            let text=`insert into datas values ($1,$2,$3,$4,$5,$6,$7,$8);`
+            client.query(text,values,(err,res)=>{
+                if (err) throw err;
+                console.log(res)
+            })
+            //database.push(user);
             res.end("Your data is stored");
         });
     }
 
-    //get
-    if(req.method=="GET" && req.url == "/find-user"){
-        res.write(JSON.stringify(database));
-        res.end()
-    }
-
-    //put
-    if(req.method == "PUT" && req.url == "/update-value"){
-        req.on("data",(data)=>{
-            user1=data;
-        });
-        req.on("end",()=>{
-            console.log("Data is modifiying"); 
-            let obj1=JSON.parse(user1);
-        database.forEach((item)=>{
-            if(obj1.id == item.id)
-            {
-                item.name=obj1.name;
-                item.role=obj1.role;
-            }
+    //get-records
+    if(req.method=="GET" && req.url == "/get-records")
+    {
+        //res.write(JSON.stringify(database));
+        let text=`select * from datas;`;
+        client.query(text,(err,data1)=>{
+            if (err) throw err;
+            console.log("data from database",data1);
+            res.write(JSON.stringify(data1.rows));
+            res.end()
         })
-        res.write("Data modified");
-        res.end();
-        });
         
     }
-    
+    if (req.method=="GET" && req.url == "/areas-to-focus")
+    {
+        let text=`select * from datas where *<=3 order by date limit 1 ;`
+        client.query(text,(err,data2)=>{
+            if (err) throw err;
+            console.log("area to focus ",data2);
+            res.write(JSON.stringify(data2.rows));
+            res.end()
+        })
+    }
+
 }).listen(8080);
 console.log("Server on");
